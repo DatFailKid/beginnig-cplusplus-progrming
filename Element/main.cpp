0d563608afc58cbcff298d8e571778787c9a6952
DatFailKid <myronstewartjr@gmail.com>
2017-07-27 10:40:14 -0400

    Added title screen Added main menu Started a bit of the game

--- Element/main.cpp
+++ Element/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 
 #define ANSI_COLOR_RED     "\x1b[31m"
 #define ANSI_COLOR_GREEN   "\x1b[32m"
@@ -11,8 +12,13 @@
 #define ANSI_COLOR_BLUE "\x1b[34m"
 #define ANSI_COLOR_GREY "\x1b[37m"
 
+using namespace std;
 void greeting();
 void title();
+void intro();
+
+char pname[15];
+char pgender=' ';
 
 int main() {
     greeting();
@@ -29,9 +35,63 @@ void greeting() {
 
 void title() {
     char command[10];
-    char chat[]="start";
+    char start[]="start";
     char settings[]="set";
-    char help[]="help";
     char end[]="exit";
     char ans=' ';
+
+    cout << "Use Enter to progress through menus" << endl;
+    cout << "Type one of the underlined following:" << endl;
+    cout << ANSI_COLOR_GREEN << ANSI_TEXT_UNDERLINE "start" ANSI_COLOR_RESET << ": Start your adventurre" << endl;
+    cout << ANSI_COLOR_GREY << ANSI_TEXT_UNDERLINE "set" ANSI_COLOR_RESET << ": Settings, change stuff..." << endl;
+    cout << ANSI_COLOR_CYAN << ANSI_TEXT_UNDERLINE "exit" ANSI_COLOR_RESET << ": Exit the game" << endl;
+    cin >> command;
+
+    if (strcmp(command, start) == 0) {
+        title();
+    }
+    if (strcmp(command, settings) == 0) {
+        getchar();
+        cout << ANSI_COLOR_RED "Not done with this yet!" ANSI_COLOR_RESET<< endl;
+        getchar();
+        title();
+
+        /* Add in:
+         * Turn highlight on/off
+         * Change text color
+         * Mouse input on/off*/
+    }
+
+    if (strcmp(command, end) == 0) {
+        cout << ANSI_COLOR_CYAN"Are you sure you want to exit?" << endl;
+        cout << "Type 'y' for 'yes', type 'n' for 'no'." ANSI_COLOR_RESET<< endl;
+        cin >> ans;
+
+        switch (ans) {
+            case 'y':
+                getchar();
+                cout << "Shutting down..." << endl;
+                getchar();
+                break;
+            case 'n':
+                getchar();
+                cout << "Oh, ok" << endl;
+                getchar();
+                title();
+            default:
+                getchar();
+                cout << "I'm assuming you mean 'no'." << endl;
+                getchar();
+                title();
+        }
+    }
+    else {
+        title();
+    }
+}
+
+void intro() {
+    cout << "You wake up to a seriously bright light" << endl;
+    cout << "A light so bright you can't even squint at it" << endl;
+    cout << "Then, gradually, you can open your eyes with a bit of ease." << endl;
 }
